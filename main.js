804e5020f03db42e0e76b188dcda79b0cf9df822
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-20 07:44:20 +0000

    Done.

--- main.js
+++ main.js
@@ -5,7 +5,31 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
 
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
+let articleHearts = document.querySelectorAll(".like");
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
+
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall("bogusUrl")
+    .then(function(serverMessage){
+       heart.innerText = glyphStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      document.getElementById("modal").className = "";
+    });
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
